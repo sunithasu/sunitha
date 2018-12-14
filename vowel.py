b79457e4177465939e81c958739bc035b42eaca7
sunithasu <45839050+sunithasu@users.noreply.github.com>
2018-12-14 15:22:16 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+str = input("Input a letter of the alphabet: ")
+ 
+if str in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'A', 'E'):
+	print("%s is a vowel." % str)
+else:
+	print("%s is a consonant." % str) 
+ 
