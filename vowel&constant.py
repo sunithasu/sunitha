02cb49373a3d5e5148c256dbaa001e797585a22b
sunithasu <45839050+sunithasu@users.noreply.github.com>
2018-12-14 13:48:02 +0530

    Create vowel&constant.py

---
+++ vowel&constant.py
@@ -0,0 +1,5 @@
+ch=input ("enter your character:")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' OR ch=='O' or ch=='U')
+print(ch,"is vowel")
+else
+print(ch,"is constant")
